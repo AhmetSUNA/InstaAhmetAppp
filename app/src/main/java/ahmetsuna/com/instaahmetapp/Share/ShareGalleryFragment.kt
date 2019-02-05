b0daba1a389292a2291a2156776fca93f3156eb6
AhmetSUNA <34107716+AhmetSUNA@users.noreply.github.com>
2019-02-05 13:24:31 +0300

    Galeri Fragmentinin Tasarlanması ve ArrayAdapter Kullanımı

--- app/src/main/java/ahmetsuna/com/instaahmetapp/Share/ShareGalleryFragment.kt
+++ app/src/main/java/ahmetsuna/com/instaahmetapp/Share/ShareGalleryFragment.kt
@@ -3,17 +3,43 @@ package ahmetsuna.com.instaahmetapp.Share
 
 import ahmetsuna.com.instaahmetapp.R
 import android.os.Bundle
+import android.os.Environment
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.fragment_share_gallery.view.*
 
 class ShareGalleryFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        return inflater.inflate(R.layout.fragment_share_gallery, container, false)
+        var view = inflater.inflate(R.layout.fragment_share_gallery, container, false)
+
+        var klasorPaths = ArrayList<String>()
+
+        var klasorAdlari = ArrayList<String>()
+
+        var root = Environment.getExternalStorageDirectory().path
+        var kameraResimleri = root + "/DCIM/Camera"
+        var indirilenResimler = root + "/Download"
+        var whatsappResimleri = root + "/WhatsApp/Media/WhatsApp Images"
+
+        klasorPaths.add(kameraResimleri)
+        klasorPaths.add(indirilenResimler)
+        klasorPaths.add(whatsappResimleri)
+
+        klasorAdlari.add("Kamera")
+        klasorAdlari.add("İndirilenler")
+        klasorAdlari.add("WhatsApp")
+
+        var spinnerArrayAdapter = ArrayAdapter(activity, android.R.layout.simple_spinner_item, klasorAdlari)
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        view.spnKlasorAdlari.adapter = spinnerArrayAdapter
+
+
+        return view
     }
-
-
 }
