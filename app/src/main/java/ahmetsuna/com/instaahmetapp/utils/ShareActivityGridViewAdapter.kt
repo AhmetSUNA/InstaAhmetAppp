70e282f5743d0853855293b7c67ef3be9d7c54d3
AhmetSUNA <34107716+AhmetSUNA@users.noreply.github.com>
2019-02-06 16:06:29 +0300

    Video Dosyalarının Sürelerini Göstermek

--- app/src/main/java/ahmetsuna/com/instaahmetapp/utils/ShareActivityGridViewAdapter.kt
+++ app/src/main/java/ahmetsuna/com/instaahmetapp/utils/ShareActivityGridViewAdapter.kt
@@ -1,13 +1,17 @@
 package ahmetsuna.com.instaahmetapp.utils
 
-import ahmetsuna.com.instaahmetapp.R
 import android.content.Context
+import android.media.MediaMetadataRetriever
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ProgressBar
+import android.widget.TextView
 import kotlinx.android.synthetic.main.tek_sutun_grid_resim.view.*
+
+
 
 class ShareActivityGridViewAdapter(context: Context, resource: Int, var klasordekiDosyalar: ArrayList<String>) : ArrayAdapter<String>(context, resource, klasordekiDosyalar) {
 
@@ -23,6 +27,7 @@ class ShareActivityGridViewAdapter(context: Context, resource: Int, var klasorde
 
         lateinit var imageView : GridImageView
         lateinit var progressBar : ProgressBar
+        lateinit var tvSure: TextView
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
@@ -30,11 +35,12 @@ class ShareActivityGridViewAdapter(context: Context, resource: Int, var klasorde
         tekSutunResim = convertView
 
         if (tekSutunResim == null){
-            tekSutunResim = inflater.inflate(R.layout.tek_sutun_grid_resim, parent, false)
+            tekSutunResim = inflater.inflate(ahmetsuna.com.instaahmetapp.R.layout.tek_sutun_grid_resim, parent, false)
             viewHolder = ViewHolder()
 
             viewHolder.imageView = tekSutunResim!!.imgTekSutunImage
             viewHolder.progressBar = tekSutunResim!!.progressBar
+            viewHolder.tvSure = tekSutunResim!!.tvSure
 
             tekSutunResim!!.setTag(viewHolder)
         }else{
@@ -43,9 +49,66 @@ class ShareActivityGridViewAdapter(context: Context, resource: Int, var klasorde
 
         }
 
-        UniversalImageLoader.setImage(klasordekiDosyalar.get(position), viewHolder.imageView, viewHolder.progressBar,"file:/")
+        var dosyaYolu = klasordekiDosyalar.get(position)
+        var dosyaTuru = dosyaYolu.substring(dosyaYolu.lastIndexOf("."))
+
+        if (dosyaTuru.equals(".mp4")){
+
+            viewHolder.tvSure.visibility = View.VISIBLE
+            var retriver= MediaMetadataRetriever()
+            retriver.setDataSource(context, Uri.parse("file://" + dosyaYolu))
+
+            var videoSuresi = retriver.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
+            var videoSuresiLong = videoSuresi.toLong()
+
+
+
+            viewHolder.tvSure.setText(sureDonustur(videoSuresiLong))
+            UniversalImageLoader.setImage(klasordekiDosyalar.get(position), viewHolder.imageView, viewHolder.progressBar,"file:/")
+
+        }else{
+
+            viewHolder.tvSure.visibility = View.GONE
+            UniversalImageLoader.setImage(klasordekiDosyalar.get(position), viewHolder.imageView, viewHolder.progressBar,"file:/")
+
+        }
+
 
         return tekSutunResim!!
+    }
+
+    //bir video veya şarkının süresini bulma
+    fun sureDonustur(duration: Long): String {
+        var out: String? = null
+        var hours: Long = 0
+        try {
+            hours = duration / 3600000
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return out!!
+        }
+
+        val remaining_minutes = (duration - hours * 3600000) / 60000
+        var minutes = remaining_minutes.toString()
+        if (minutes.equals("0")) {
+            minutes = "00"
+        }
+        val remaining_seconds = duration - hours * 3600000 - remaining_minutes * 60000
+        var seconds = remaining_seconds.toString()
+        if (seconds.length < 2) {
+            seconds = "00"
+        } else {
+            seconds = seconds.substring(0, 2)
+        }
+
+        if (hours > 0) {
+            out = "$hours:$minutes:$seconds"
+        } else {
+            out = "$minutes:$seconds"
+        }
+
+        return out
+
     }
 
 }
