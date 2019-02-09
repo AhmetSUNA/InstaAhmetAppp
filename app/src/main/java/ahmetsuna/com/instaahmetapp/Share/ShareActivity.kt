c89029fb9a0633e5dc110335eddfe7647a33d0f3
AhmetSUNA <34107716+AhmetSUNA@users.noreply.github.com>
2019-02-09 17:59:45 +0300

    Secilen Resmin Storage ve Veritabanına Kaydedilmesi

--- app/src/main/java/ahmetsuna/com/instaahmetapp/Share/ShareActivity.kt
+++ app/src/main/java/ahmetsuna/com/instaahmetapp/Share/ShareActivity.kt
@@ -1,10 +1,13 @@
 package ahmetsuna.com.instaahmetapp.Share
 
+import ahmetsuna.com.instaahmetapp.Login.LoginActivity
 import ahmetsuna.com.instaahmetapp.R
 import ahmetsuna.com.instaahmetapp.utils.SharePagerAdapter
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_share.*
 
 class ShareActivity : AppCompatActivity() {
@@ -12,9 +15,14 @@ class ShareActivity : AppCompatActivity() {
     private val ACTIVITY_NO=2
     private val TAG="ShareActivity"
 
+    lateinit var myAuthListener: FirebaseAuth.AuthStateListener
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_share)
+
+        setupAuthListener()
 
         setupShareViewPager()
 
@@ -45,6 +53,29 @@ class ShareActivity : AppCompatActivity() {
         anaLayout.visibility = View.VISIBLE
         fragmenContainerLayout.visibility = View.GONE
         super.onBackPressed()
+    }
+
+    private fun setupAuthListener() {
+
+        //bu tetiklenmeden buna ulaşmaya çalışırsak nullPointException çıkar dikkat!!!
+        myAuthListener = object : FirebaseAuth.AuthStateListener {
+            override fun onAuthStateChanged(p0: FirebaseAuth) {
+                var user = FirebaseAuth.getInstance().currentUser
+
+                //kullanıcı null ise
+                if (user == null) {
+                    //çıkış yapıldığında loginActivity'e yolla
+                    var intent = Intent(this@ShareActivity, LoginActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                    startActivity(intent)
+                    finish()
+                } else {
+
+
+                }
+            }
+
+        }
     }
 
 }
--- app/src/main/java/ahmetsuna/com/instaahmetapp/Share/ShareNextFragment.kt
+++ app/src/main/java/ahmetsuna/com/instaahmetapp/Share/ShareNextFragment.kt
@@ -1,16 +1,33 @@
 package ahmetsuna.com.instaahmetapp.Share
 
 
+import ahmetsuna.com.instaahmetapp.Models.Posts
+import ahmetsuna.com.instaahmetapp.Profile.YukleniyorFragment
 import ahmetsuna.com.instaahmetapp.R
 import ahmetsuna.com.instaahmetapp.utils.EventBusDataEvents
 import ahmetsuna.com.instaahmetapp.utils.UniversalImageLoader
 import android.content.Context
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ServerValue
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.OnProgressListener
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
+import kotlinx.android.synthetic.main.fragment_share_next.*
 import kotlinx.android.synthetic.main.fragment_share_next.view.*
+import kotlinx.android.synthetic.main.fragment_yukleniyor.*
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 
@@ -18,6 +35,12 @@ import org.greenrobot.eventbus.Subscribe
 class ShareNextFragment : Fragment() {
 
     var secilenResimYolu:String? = null
+    lateinit var fileURI: Uri
+
+    lateinit var myAuth: FirebaseAuth
+    lateinit var myUser: FirebaseUser
+    lateinit var myRef: DatabaseReference
+    lateinit var myStorageReference: StorageReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,7 +51,64 @@ class ShareNextFragment : Fragment() {
 
         UniversalImageLoader.setImage(secilenResimYolu!!, view!!.imgSecilenResim, null, "file://")
 
+        fileURI = Uri.parse("file://" + secilenResimYolu)
+
+        myAuth = FirebaseAuth.getInstance()
+        myUser = myAuth.currentUser!!
+        myRef = FirebaseDatabase.getInstance().reference
+        myStorageReference = FirebaseStorage.getInstance().reference
+
+            view.tvPaylasButton.setOnClickListener {
+
+            var dialogYukleniyor = YukleniyorFragment()
+            dialogYukleniyor.show(activity!!.supportFragmentManager, "yukleniyor")
+            dialogYukleniyor.isCancelable = false
+
+            var ref =myStorageReference.child("users").child(myUser.uid).child(fileURI.lastPathSegment)
+
+            var uploadTask = ref.putFile(fileURI)
+            val urlTask = uploadTask.continueWithTask(Continuation<UploadTask.TaskSnapshot, Task<Uri>> { task ->
+                if (!task.isSuccessful) {
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                return@Continuation ref.downloadUrl
+            }).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val downloadUri = task.result
+                    dialogYukleniyor.dismiss()
+                    veritabaninaBilgileriYaz(downloadUri.toString())
+
+                } else {
+                    dialogYukleniyor.dismiss()
+                    Toast.makeText(activity, "Hata oluştu" + task.exception!!.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+                uploadTask.addOnProgressListener ( object : OnProgressListener<UploadTask.TaskSnapshot> {
+                    override fun onProgress(p0: UploadTask.TaskSnapshot?) {
+
+                        var progress = 100.0 * p0!!.bytesTransferred / p0!!.totalByteCount
+                        dialogYukleniyor.tvBilgi.text = "%" + progress.toInt().toString() + "yüklendi.."
+                    }
+                } )
+
+
+
+        }
+
         return view
+    }
+
+    private fun veritabaninaBilgileriYaz(yuklenenFotoURL: String) {
+
+        var postID = myRef.child("posts").child(myUser.uid).push().key
+
+        var yuklenenPost = Posts(myUser.uid, postID,"",etPostAciklama.text.toString(), yuklenenFotoURL)
+
+        myRef.child("posts").child(myUser.uid).child(postID!!).setValue(yuklenenPost)
+        myRef.child("posts").child(myUser.uid).child(postID).child("yuklenme_tarih").setValue(ServerValue.TIMESTAMP)
+
     }
 
     ///////////////////////////////////EVENTBUS///////////////////////////////
