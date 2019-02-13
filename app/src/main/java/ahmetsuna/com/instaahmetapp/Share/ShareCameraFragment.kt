af0672b6f0e68c6763ef9f92792c63565f9e1ffc
AhmetSUNA <34107716+AhmetSUNA@users.noreply.github.com>
2019-02-13 23:51:14 +0300

    Camera ve Video Fragmentlerinin Arayüzlerinin Oluşturulması

--- app/src/main/java/ahmetsuna/com/instaahmetapp/Share/ShareCameraFragment.kt
+++ app/src/main/java/ahmetsuna/com/instaahmetapp/Share/ShareCameraFragment.kt
@@ -4,16 +4,43 @@ package ahmetsuna.com.instaahmetapp.Share
 import ahmetsuna.com.instaahmetapp.R
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.otaliastudios.cameraview.CameraView
+import kotlinx.android.synthetic.main.fragment_share_video.view.*
 
 class ShareCameraFragment : Fragment() {
 
+    lateinit var cameraView : CameraView
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        return inflater.inflate(R.layout.fragment_share_camera, container, false)
+        var view = inflater.inflate(R.layout.fragment_share_camera, container, false)
+
+        cameraView = view.videoView
+
+
+
+        return view
     }
 
+    override fun onResume() {
+        super.onResume()
+        Log.e("HATA2", "CAMERA FRAGMENTİ ON RESUME")
+        cameraView.start()
+    }
 
+    override fun onPause() {
+        super.onPause()
+        Log.e("HATA2", "CAMERA FRAGMENTİ ON PAUSE")
+        cameraView.stop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("HATA2", "CAMERA FRAGMENTİ ON DESTROY")
+        cameraView.destroy()
+    }
 }
--- app/src/main/java/ahmetsuna/com/instaahmetapp/Share/ShareVideoFragment.kt
+++ app/src/main/java/ahmetsuna/com/instaahmetapp/Share/ShareVideoFragment.kt
@@ -4,15 +4,44 @@ package ahmetsuna.com.instaahmetapp.Share
 import ahmetsuna.com.instaahmetapp.R
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.otaliastudios.cameraview.CameraView
+import kotlinx.android.synthetic.main.fragment_share_video.view.*
 
 class ShareVideoFragment : Fragment() {
 
+    lateinit var videoView : CameraView
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        return inflater.inflate(R.layout.fragment_share_video, container, false)
+        var view =  inflater.inflate(R.layout.fragment_share_video, container, false)
+
+        videoView = view.videoView
+
+        return view
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e("HATA2", "VİDEO FRAGMENTİ ON RESUME")
+        videoView.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e("HATA2", "VİDEO FRAGMENTİ ON PAUSE")
+        videoView.stop()
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("HATA2", "VİDEO FRAGMENTİ ON DESTROY")
+        videoView.destroy()
+
     }
 
 
