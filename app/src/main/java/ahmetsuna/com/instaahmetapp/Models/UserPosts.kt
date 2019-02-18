02798c593b76d8754bec0a5ef60cda6f7cc7e493
AhmetSUNA <34107716+AhmetSUNA@users.noreply.github.com>
2019-02-18 14:28:41 +0300

    HomeFragmentda Kullanıcı Gönderileri için Layout ve Model Oluşturulması

---
+++ app/src/main/java/ahmetsuna/com/instaahmetapp/Models/UserPosts.kt
@@ -0,0 +1,12 @@
+package ahmetsuna.com.instaahmetapp.Models
+
+class UserPosts {
+
+    var userID: String? = null
+    var userName: String? = null
+    var userFotoURL: String? = null
+    var postID: String? = null
+    var postAciklama: String? = null
+    var postURL: String? = null
+
+}
